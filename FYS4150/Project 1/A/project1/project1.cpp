2c124b5166658616450cd24305746661218f1e65
Eimund <eimund.smestad@gmail.com>
2014-09-01 23:53:37 +0200

    Forbredet cutoff i tridiagonal matrise løseren

--- FYS4150/Project 1/A/project1/project1.cpp
+++ FYS4150/Project 1/A/project1/project1.cpp
@@ -24,17 +24,17 @@ template<class T> class GetPointer {
 template<class T> class GetPointer<T*> {
     public: typedef T Type;                             // Used to get the type the pointer points to at compile time
 };
-template<unsigned int Type, class V> class TridiagonalMatrix {              // General tridiagonal matrix solver
-    public: static void Solve(V* a, V* b, V* c, V* f, unsigned int n) {     // 8n FLOPS
+template<unsigned int Type, class T> class TridiagonalMatrix {              // General tridiagonal matrix solver
+    public: static void Solve(T* a, T* b, T* c, T* f, unsigned int n) {     // 8n FLOPS
         /* a are elements a_{i,i-1} in tridiagonal matrix, a[0] = a_{2,1}
          * b are elements a_{i,i} in tridiagonal matrix, b[0] = a_{1,1}
          * c are elements a_{i,i+1} in tridiagonal matrix, c[0] = a_{1,2}
          * f are source elements b_{i}, f[0] = b_{1}
          */
 
-        V temp;
-        V* start = f;
-        V* end = &f[n-1];
+        T temp;
+        T* start = f;
+        T* end = &f[n-1];
         while(f < end) {
             temp = (*a++)/(*b++);
             *b -= temp*(*c++);          // Eq (4) in report
@@ -60,67 +60,63 @@ template<unsigned int Type, class V> class TridiagonalMatrix {              // G
          */
     }
 };
-template<class V> class TridiagonalMatrix<1,V> {                            // Tridiagonal matrix -1,2,-1 without memory usage
-    public: static void Solve(V* f, unsigned int n) {                       // 6n FLOPS
+template<class T> class TridiagonalMatrix<1,T> {                            // Tridiagonal matrix -1,2,-1 without memory usage
+    public: static void Solve(T* f, unsigned int n) {                       // 6n FLOPS => 2n FLOPS when n >> cutoff
         Solve(f, n, n);
     }
-    public: static void Solve(V* f, unsigned int n, unsigned int cutoff) {
+    public: static void Solve(T* f, unsigned int n, unsigned int cutoff) {
         /*
          * f are source elements b_{i}, f[0] = b_{1}
          * The values of i >= cutoff is when (i+1)/i is approximated to 1
          */
 
-        V* start = f;
-        V* mid = &f[cutoff-1];
-        V* end = &f[n-1];
+        T* start = f;
+        T* mid = &f[n < cutoff ? n-1 : cutoff-1];
+        T* end = &f[n-1];
         unsigned int i = 1;
         while(f < mid)
-            *f += (V)i++/i*(*f++);      // Eq (8)
+            *f += (T)i++/i*(*f++);      // Eq (8)
         while(f < end)
             *f += *f++;                 // Eq (8) with (i+1)/i = 1
         i++;
         while(f > mid)
             *f += *f--;                 // Eq (10) with (i+1)/i = 1
         while(f > start) {
-            *f /= (V)i--/i;             // Eq (11)
+            *f /= (T)i--/i;             // Eq (11)
             *f += *f--;                 // Eq (10)
         }
         *f /= 2;
     }
 };
-template<class V> class TridiagonalMatrix<2,V> {                            // Tridiagonal matrix -1,2,-1 with memory usage
-    public: V* factor;                   // Precalculated values
-    public: class _cutoff{               // Number of precalculated values
-        private: TridiagonalMatrix<2,V>* owner;
-        private: unsigned int value;
-        public: _cutoff() {
+template<class T> class TridiagonalMatrix<2,T> {                            // Tridiagonal matrix -1,2,-1 with memory usage
+    public: T* factor;                   // Precalculated values
+    private: unsigned int _cutoff;
+    public: class _cutoff_{              // property class
+        private: TridiagonalMatrix<2,T>* owner;
+        public: _cutoff_() {
         }
-        public: _cutoff(TridiagonalMatrix<2,V>* owner) {
-            value = 0;
+        public: _cutoff_(TridiagonalMatrix<2,T>* owner) {
             this->owner = owner;
+            this->owner->_cutoff = 0;
         }
-        public: ~_cutoff() {
-            if(value)
-                delete [] owner->factor;
-        }
-        public: unsigned int & operator = (const unsigned int &i) {
-            if(value != i) {
-                V* arr = new V[i];
-                V* start;
-                if(value < i) {                     // Extend values
-                    start = &arr[value];
-                    arr = &arr[i-1];
-                    unsigned int j = i+1;
+        public: unsigned int & operator = (const unsigned int& cutoff) { // set function
+            if(owner->_cutoff != cutoff) {
+                T* arr = new T[cutoff];
+                T* start;
+                if(owner->_cutoff < cutoff) {       // Extend values
+                    start = &arr[owner->_cutoff];
+                    arr = &arr[cutoff-1];
+                    unsigned int i = cutoff+1;
                     while(arr >= start)
-                        *arr-- = (V)j--/j;          // Calculate (i+1)/i values
+                        *arr-- = (T)i--/i;          // Calculate (i+1)/i values
                 }
-                if(value != 0) {
+                if(owner->_cutoff!= 0) {
                     start = owner->factor;
-                    if(value < i)
-                        owner->factor= &owner->factor[value-1];
+                    if(owner->_cutoff < cutoff)
+                        owner->factor= &owner->factor[owner->_cutoff-1];
                     else {
-                        arr = &arr[i-1];
-                        owner->factor= &owner->factor[i-1];
+                        arr = &arr[cutoff-1];
+                        owner->factor= &owner->factor[cutoff-1];
                     }
                     while(owner->factor >= start)   // Copy already calculated values
                         *arr-- = *(owner->factor--);
@@ -128,28 +124,28 @@ template<class V> class TridiagonalMatrix<2,V> {                            // T
                 }
                 owner->factor = ++arr;
             }
-            return value = i;
+            return owner->_cutoff= cutoff;
         }
-        public: operator unsigned int () const {
-            return value;
+        public: operator unsigned int () const {    // get function
+            return owner->_cutoff;
         }
-    }cutoff;
+    } cutoff;   // Number of precalculated values
     public: TridiagonalMatrix(unsigned int cutoff) {
-        this->cutoff = _cutoff(this);
+        this->cutoff = _cutoff_(this);
         this->cutoff = cutoff;
     }
     public: ~TridiagonalMatrix() {
+        delete [] factor;
     }
-    public: void Solve(V* f, unsigned int n) {         // 4n FLOPS
+    public: void Solve(T* f, unsigned int n) {         // 4n FLOPS => 2n FLOPS for n >> cutoff
         /*
          * f are source elements b_{i}, f[0] = b_{1}
          */
 
-        V* start = f;
-        V* mid = &f[cutoff-1];
-        V* end = &f[n-1];
-        V* fac = factor;
-        unsigned int i = 1;
+        T* start = f;
+        T* mid = &f[n < _cutoff ? n-1 : _cutoff-1];
+        T* end = &f[n-1];
+        T* fac = factor;
         while(f < mid)
             *f += (*f++)/(*fac++);      // Eq (8)
         while(f < end)
@@ -160,7 +156,7 @@ template<class V> class TridiagonalMatrix<2,V> {                            // T
             *f /= *fac--;               // Eq (11)
             *f += *f--;                 // Eq (10)
         }
-        *f /= 2;
+        *f /= 2.0;
     }
 };
 
@@ -189,7 +185,7 @@ int main() {
         delete [] f;
         f = f_tmp;
         f_tmp = CopyOfArray(f, n[i]);
-        TridiagonalMatrix<1,GetPointer<decltype(f)>::Type>::Solve(f, n[i]);             // Solve -1,2,-1 tridiagonal matrix without memory usage
+        TridiagonalMatrix<1,GetPointer<decltype(f)>::Type>::Solve(f, n[i],n[i]);             // Solve -1,2,-1 tridiagonal matrix without memory usage
 
         delete [] f;
         f = f_tmp;
