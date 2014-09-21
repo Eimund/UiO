1cb584f37cc88db67b0e31e665a20c81c0d41cae
Eimund <eimund.smestad@gmail.com>
2014-09-21 02:38:17 +0200

    Implementert Jacobi

--- FYS4150/Project 2/project2/Matrix.h
+++ FYS4150/Project 2/project2/Matrix.h
@@ -11,21 +11,37 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include <cmath>
+#include <iostream>
 #include "armadillo"
 #include "lib.cpp"
 
 using namespace arma;
+using namespace std;
 
 enum class MatrixType {
+    Square,
     Symmetric,
     Tridiagonal,
     Tridiagonal_m1_2_m1,
     Tridiagonal_m1_2_m1_6n,
     Tridiagonal_m1_2_m1_4n,
-    LU_decomposition
+    LU_decomposition,
+    tqli
 };
+typedef struct {
+    unsigned int i, j;
+} MatrixIndex;
 
 template<MatrixType Type, class T> class Matrix;
+template<MatrixType Type, class T> void MatrixCout(Matrix<Type, T>& matrix) {
+    for(unsigned int i = 0; i < matrix.n; i++) {
+        for(unsigned int j = 0; j < matrix.n; j++)
+            cout << matrix(i,j) << "\t";
+        cout << '\n';
+    }
+    cout << '\n';
+}
 template<class M, class T> class MatrixDiagonal {
     private: M* owner;
     private: template<class P> class Type {
@@ -66,11 +82,11 @@ template<class M, class T> class MatrixDiagonal {
     }
 };
 template <class M, class T> class MatrixElements {
-    private: T** matrix;
-    private: unsigned int _n;
+    protected: T** matrix;
+    protected: unsigned int _n;
     private: class _n_ {             // property class
-        private: M* owner;
-        public: _n_(M* owner) : owner(owner) {
+        private: MatrixElements<M, T>* owner;
+        public: _n_(MatrixElements<M, T>* owner) : owner(owner) {
             this->owner->_n = 1;
             owner->matrix = new T*[1];
             owner->matrix[0] = new T[1];
@@ -82,12 +98,12 @@ template <class M, class T> class MatrixElements {
                     matrix[i] = new T[n];
                 unsigned int n_ = n < owner->_n ? n : owner->_n;
                 for(unsigned int i = 0; i < n_; i++) {
-                    for(unsigned int j = 0; j < n; j++)
+                    for(unsigned int j = 0; j < n_; j++)
                         matrix[i][j] = owner->matrix[i][j];
                 }
-                for(unsigned int i = 0; owner->_n; i++)
-                    delete [] matrix[i];
-                delete [] matrix;
+                for(unsigned int i = 0; i < owner->_n; i++)
+                    delete [] owner->matrix[i];
+                delete [] owner->matrix;
                 owner->matrix = matrix;
                 return owner->_n = n;
             } else
@@ -106,13 +122,16 @@ template <class M, class T> class MatrixElements {
             delete [] matrix[i];
         delete [] matrix;
     }
-    public: T& operator() (const unsigned int row, const unsigned int col) { // Matrix indexing
+    public: inline T& operator() (const unsigned int& row, const unsigned int& col) { // Matrix indexing
         return matrix[row][col];
+    }
+    public: inline operator T**() const {
+        return matrix;
     }
 };
 template <class T> class MatrixElements<Matrix<MatrixType::Symmetric, T>, T> {
-    private: T** matrix;
-    private: unsigned int _n;
+    protected: T** matrix;
+    protected: unsigned int _n;
     private: class _n_ {             // property class
         private: MatrixElements<Matrix<MatrixType::Symmetric, T>, T>* owner;
         public: _n_(MatrixElements<Matrix<MatrixType::Symmetric, T>, T>* owner) : owner(owner) {
@@ -151,8 +170,66 @@ template <class T> class MatrixElements<Matrix<MatrixType::Symmetric, T>, T> {
             delete [] matrix[i];
         delete [] matrix;
     }
-    public: T& operator() (const unsigned int row, const unsigned int col) { // Matrix indexing
-        return row < col ? matrix[row][col-row] : matrix[col][row-col];
+    public: inline T& operator() (const unsigned int& row, const unsigned int& col) { // Matrix indexing
+        return row < col ? matrix[col-row][row] : matrix[row-col][col];
+    }
+    public: inline operator T**() const {
+        return matrix;
+    }
+};
+template <class T> class MatrixElements<Matrix<MatrixType::tqli, T>, T> {
+    protected: T** matrix;
+    protected: unsigned int _n;
+    private: class _n_ {             // property class
+        private: MatrixElements<Matrix<MatrixType::tqli, T>, T>* owner;
+        public: _n_(MatrixElements<Matrix<MatrixType::tqli, T>, T>* owner) : owner(owner) {
+            this->owner->_n = 1;
+            owner->matrix = new T*[1];
+            owner->matrix[0] = new T[1];
+        }
+        public: unsigned int& operator = (const unsigned int& n) { // set function
+            if(n && n != owner->_n) {
+                T** matrix = new T*[n];
+                for(unsigned int i = 0; i < n; i++) {
+                    if(i == 1) {
+                        matrix[1] = new T[n];           // Bug i tqli i lib.cpp, trenger ett ekstra element
+                        matrix[1] = &matrix[1][1];
+                    }
+                    else
+                        matrix[i] = new T[n-i];
+                }
+                unsigned int n_ = n < owner->_n ? n : owner->_n;
+                for(unsigned int i = 0; i < n_; i++) {
+                    for(unsigned int j = 0; j < n_-i; j++)
+                        matrix[i][j] = owner->matrix[i][j];
+                }
+                for(unsigned int i = 0; i < owner->_n; i++)
+                    delete [] owner->matrix[i];
+                delete [] owner->matrix;
+                owner->matrix = matrix;
+                return owner->_n = n;
+            } else
+                return owner->_n;
+        }
+        public: operator unsigned int () const {                    // get function
+            return owner->_n;
+        }
+    };
+    public: _n_ n;
+    protected: MatrixElements(unsigned int n) : n(this) {
+        this->n = n;
+    }
+    public: ~MatrixElements() {
+        for(unsigned int i = 0; i < _n; i++) {
+            if(i == 1)
+                delete [] &matrix[1][-1];           // Bug i tqli i lib.cpp,
+            else
+                delete [] matrix[i];
+        }
+        delete [] matrix;
+    }
+    public: inline T& operator() (const unsigned int& row, const unsigned int& col) { // Matrix indexing
+        return row < col ? matrix[col-row][row] : matrix[row-col][col];
     }
 };
 template <class T> class MatrixElements<Matrix<MatrixType::Tridiagonal, T>, T> {
@@ -225,12 +302,263 @@ template <class T> class MatrixElements<Matrix<MatrixType::Tridiagonal_m1_2_m1, 
         return 0;
     }
 };
+template<class T> class Matrix<MatrixType::Square, T> :
+        public MatrixDiagonal<Matrix<MatrixType::Square, T>, T>,
+        public MatrixElements<Matrix<MatrixType::Square, T>, T> {
+    public: Matrix(unsigned int n) :
+        MatrixDiagonal<Matrix<MatrixType::Square, T>, T>(this),
+        MatrixElements<Matrix<MatrixType::Square, T>, T>(n) {
+
+    }
+    void JacobiMethod(T error, unsigned int n) {
+        MatrixIndex I;
+        T s, c, t, tau, a_kk, a_ll, a_ik, a_il;
+        while(error < MaxAbs(this->matrix, this->_n, I)) {
+            tau = (this->matrix[I.j][I.j] - this->matrix[I.i][I.i])/(2*this->matrix[I.i][I.j]);
+            if (tau > 0)
+                t = 1.0/(tau + sqrt(1.0 + tau*tau));
+            else
+                t = -1.0/( -tau + sqrt(1.0 + tau*tau));
+            c = 1/sqrt(1+t*t);
+            s = c*t;
+
+            /*if(!(--n))
+                cout << "i = " << I.i << "\t j = " << I.j << "\taij = " << this->matrix[I.i][I.j] << "\tsin = " << s << "\tcos = " << c << "\t" << endl << endl;*/
+
+            a_kk = this->matrix[I.i][I.i];
+            a_ll = this->matrix[I.j][I.j];
+            // changing the matrix elements with indices k and l
+            this->matrix[I.i][I.i] = c*c*a_kk - 2.0*c*s*this->matrix[I.i][I.j] + s*s*a_ll;
+            this->matrix[I.j][I.j] = s*s*a_kk + 2.0*c*s*this->matrix[I.i][I.j] + c*c*a_ll;
+            this->matrix[I.i][I.j] = 0.0; // hard-coding of the zeros
+            this->matrix[I.j][I.i] = 0.0;
+            // and then we change the remaining elements
+            for (int i = 0; i < this->_n; i++ ) {
+                if ( i != I.i && i != I.j ) {
+                    a_ik = this->matrix[i][I.i];
+                    a_il = this->matrix[i][I.j];
+                    this->matrix[i][I.i] = c*a_ik + s*a_il;
+                    this->matrix[I.i][i] = this->matrix[i][I.i];
+                    this->matrix[i][I.j] = c*a_il - s*a_ik;
+                    this->matrix[I.j][i] = this->matrix[i][I.j];
+                }
+            }
+            /*if(!n)
+                MatrixCout(*this);*/
+        }
+    }
+    public: T MaxAbs(T** matrix, unsigned int n, MatrixIndex& I) {
+        T max = 0, abs1;
+        for(unsigned int i = 0, j; i < n; i++) {
+            for(j = i+1; j < n; j++) {
+                abs1 = abs(matrix[i][j]);
+                if(abs1 > max) {
+                    max = abs1;
+                    I.i = i;            // I.i is the diagonal number = col-row
+                    I.j = j;            // I.j is the row number
+                }
+            }
+        }
+        return max;
+    }
+};
 template<class T> class Matrix<MatrixType::Symmetric, T> :
         public MatrixDiagonal<Matrix<MatrixType::Symmetric, T>, T>,
         public MatrixElements<Matrix<MatrixType::Symmetric, T>, T> {
     public: Matrix(unsigned int n) :
             MatrixDiagonal<Matrix<MatrixType::Symmetric, T>, T>(this),
             MatrixElements<Matrix<MatrixType::Symmetric, T>, T>(n) {
+    }
+    public: T* JacobiMethod(T error, unsigned int m) {
+        MatrixIndex I;
+        T a, b, t, s2, s, c;
+        T** offdiagonal = &this->matrix[1];
+        unsigned int k, l, n, offn = this->_n-1;
+
+        while(error < MaxAbs(offdiagonal, offn, I)) {   // Find the maximum element, and stop when error is smaller than desired
+
+            b = 2*this->matrix[++I.i][I.j];
+            this->matrix[I.i][I.j] = 0;     // bij=bij=0
+
+            I.i += I.j;                     // now I.j = i in aii, and I.i = j in ajj
+
+            a = this->matrix[0][I.i] - this->matrix[0][I.j];  // Calculate cos and sin
+            t = a/b;
+            if(t > 0)
+                t = (T)1/(t+sqrt(1+t*t));
+            else
+                t = (T)1/(t-sqrt(1+t*t));
+            s2 = (T)1/(1+t*t);
+            s = sqrt(s2);
+            c = t*s;
+
+            //if(!(--m))
+            //    cout << "i = " << I.j << "\t j = " << I.i << "\taij = " << this->matrix[I.i-I.j][I.j] << "\tsin = " << s << "\tcos = " << c << "\t" << endl << endl;
+
+            a = a*s2 + b*s*c;
+            this->matrix[0][I.j] += a;       // bii eq(9)
+            this->matrix[0][I.i] -= a;       // bjj eq(10)
+
+            for(k = I.j, l = I.i, n = 0; n < I.j; k--, l--, n++) {     // Calculate bik and bjk, row < I.j and row < I.i
+                a = this->matrix[k][n];
+                b = this->matrix[l][n];
+                this->matrix[k][n] = a*c - b*s;
+                this->matrix[l][n] = b*c + a*s;
+            }
+            for(k++, l--, n++; n < I.i; k++, l--, n++) {         // row > I.j and row < I.i
+                a = this->matrix[k][I.j];
+                b = this->matrix[l][n];
+                this->matrix[k][I.j] = a*c - b*s;
+                this->matrix[l][n] = b*c + a*s;
+            }
+            for(k++, l++, n = this->_n-I.i; l < n; k++, l++) {   // row > I.j and row > I.i
+                a = this->matrix[k][I.j];
+                b = this->matrix[l][I.i];
+                this->matrix[k][I.j] = a*c - b*s;
+                this->matrix[l][I.i] = b*c + a*s;
+            }
+            //if(!m)
+            //    MatrixCout(*this);
+        }
+        return this->matrix[0];
+    }
+    public: T* JacobiMethodFaster(T error, unsigned int m) {
+        bool run = true;
+        MatrixIndex I;
+        T a, b, t, s2, s, c;
+        unsigned int k, l, n;
+        while(run) {
+            run = false;
+            for(unsigned int i = 1, j, p = this->_n-1; p; i++, p--) {
+                for(j = 0; j < p; j++) {
+                    if(error < abs(this->matrix[i][j])) {
+                        run = true;
+
+                        I.i = i;
+                        I.j = j;
+                        b = 2*this->matrix[I.i][I.j];
+                        this->matrix[I.i][I.j] = 0;     // bij=bij=0
+
+                        I.i += I.j;                     // now I.j = i in aii, and I.i = j in ajj
+
+                        a = this->matrix[0][I.i] - this->matrix[0][I.j];  // Calculate cos and sin
+                        t = a/b;
+                        if(t > 0)
+                            t = (T)1/(t+sqrt(1+t*t));
+                        else
+                            t = (T)1/(t-sqrt(1+t*t));
+                        s2 = (T)1/(1+t*t);
+                        s = sqrt(s2);
+                        c = t*s;
+
+                        //if(!(--m))
+                        //    cout << "i = " << I.j << "\t j = " << I.i << "\taij = " << this->matrix[I.i-I.j][I.j] << "\tsin = " << s << "\tcos = " << c << "\t" << endl << endl;
+
+                        a = a*s2 + b*s*c;
+                        this->matrix[0][I.j] += a;       // bii eq(9)
+                        this->matrix[0][I.i] -= a;       // bjj eq(10)
+
+                        for(k = I.j, l = I.i, n = 0; n < I.j; k--, l--, n++) {     // Calculate bik and bjk, row < I.j and row < I.i
+                            a = this->matrix[k][n];
+                            b = this->matrix[l][n];
+                            this->matrix[k][n] = a*c - b*s;
+                            this->matrix[l][n] = b*c + a*s;
+                        }
+                        for(k++, l--, n++; n < I.i; k++, l--, n++) {         // row > I.j and row < I.i
+                            a = this->matrix[k][I.j];
+                            b = this->matrix[l][n];
+                            this->matrix[k][I.j] = a*c - b*s;
+                            this->matrix[l][n] = b*c + a*s;
+                        }
+                        for(k++, l++, n = this->_n-I.i; l < n; k++, l++) {   // row > I.j and row > I.i
+                            a = this->matrix[k][I.j];
+                            b = this->matrix[l][I.i];
+                            this->matrix[k][I.j] = a*c - b*s;
+                            this->matrix[l][I.i] = b*c + a*s;
+                        }
+                        /*if(!m)
+                            MatrixCout(*this);*/
+                    }
+                }
+            }
+        }
+        return this->matrix[0];
+    }
+    public: T* JacobiMethodFastest(T error, unsigned int m) {
+        bool run = true;
+        MatrixIndex I;
+        T a, b, t, s2, s, c;
+        unsigned int k, l, n;
+        while(run) {
+            run = false;
+            for(unsigned int i = this->_n-1, j, p = 1; i; i--, p++) {
+                for(j = 0; j < p; j++) {
+                    if(error < abs(this->matrix[i][j])) {
+                        run = true;
+
+                        I.i = i;
+                        I.j = j;
+                        b = 2*this->matrix[I.i][I.j];
+                        this->matrix[I.i][I.j] = 0;     // bij=bij=0
+
+                        I.i += I.j;                     // now I.j = i in aii, and I.i = j in ajj
+
+                        a = this->matrix[0][I.i] - this->matrix[0][I.j];  // Calculate cos and sin
+                        t = a/b;
+                        if(t > 0)
+                            t = (T)1/(t+sqrt(1+t*t));
+                        else
+                            t = (T)1/(t-sqrt(1+t*t));
+                        s2 = (T)1/(1+t*t);
+                        s = sqrt(s2);
+                        c = t*s;
+
+                        //if(!(--m))
+                        //    cout << "i = " << I.j << "\t j = " << I.i << "\taij = " << this->matrix[I.i-I.j][I.j] << "\tsin = " << s << "\tcos = " << c << "\t" << endl << endl;
+
+                        a = a*s2 + b*s*c;
+                        this->matrix[0][I.j] += a;       // bii eq(9)
+                        this->matrix[0][I.i] -= a;       // bjj eq(10)
+
+                        for(k = I.j, l = I.i, n = 0; n < I.j; k--, l--, n++) {     // Calculate bik and bjk, row < I.j and row < I.i
+                            a = this->matrix[k][n];
+                            b = this->matrix[l][n];
+                            this->matrix[k][n] = a*c - b*s;
+                            this->matrix[l][n] = b*c + a*s;
+                        }
+                        for(k++, l--, n++; n < I.i; k++, l--, n++) {         // row > I.j and row < I.i
+                            a = this->matrix[k][I.j];
+                            b = this->matrix[l][n];
+                            this->matrix[k][I.j] = a*c - b*s;
+                            this->matrix[l][n] = b*c + a*s;
+                        }
+                        for(k++, l++, n = this->_n-I.i; l < n; k++, l++) {   // row > I.j and row > I.i
+                            a = this->matrix[k][I.j];
+                            b = this->matrix[l][I.i];
+                            this->matrix[k][I.j] = a*c - b*s;
+                            this->matrix[l][I.i] = b*c + a*s;
+                        }
+                        /*if(!m)
+                            MatrixCout(*this);*/
+                    }
+                }
+            }
+        }
+        return this->matrix[0];
+    }
+    public: T MaxAbs(T** matrix, unsigned int n, MatrixIndex& I) {
+        T max = 0, abs1;
+        for(unsigned int i = 0, j; n; i++, n--) {
+            for(j = 0; j < n; j++) {
+                abs1 = abs(matrix[i][j]);
+                if(abs1 > max) {
+                    max = abs1;
+                    I.i = i;            // I.i is the diagonal number = col-row
+                    I.j = j;            // I.j is the row number
+                }
+            }
+        }
+        return max;
     }
 };
 template<class T> class Matrix<MatrixType::Tridiagonal, T> :
@@ -453,6 +781,18 @@ template<class T> class Matrix<MatrixType::LU_decomposition, T> :
             return true;
         }
         return false;
+    }
+};
+template<class T> class Matrix<MatrixType::tqli, T> :
+        public MatrixDiagonal<Matrix<MatrixType::tqli, T>, T>,
+        public MatrixElements<Matrix<MatrixType::tqli, T>, T> {
+    public: Matrix(unsigned int n) :
+        MatrixDiagonal<Matrix<MatrixType::tqli, T>, T>(this),
+        MatrixElements<Matrix<MatrixType::tqli, T>, T>(n) {
+    }
+    public: T* tqli(T** z) {
+        ::tqli(this->matrix[0], &this->matrix[1][-1], this->_n, z);
+        return this->matrix[0];
     }
 };
 #endif // MATRIX_H
--- FYS4150/Project 2/project2/project2.cpp
+++ FYS4150/Project 2/project2/project2.cpp
@@ -10,6 +10,7 @@
 
 // Include files
 #include <cmath>
+#include <iostream>
 #include <fstream>
 #include "time.h"
 
@@ -29,13 +30,16 @@ using namespace std;
 
 // Declartion of global functions
 template<class T> T* CopyOfArray(T* arr, unsigned int n);
+//template<MatrixType Type, class T> void CoutMatrix(Matrix<Type, T>& matrix);
 template<class T> T RelativeError(T* u, T* v, unsigned int n);
 template<class T> void WriteArrayToFile(ofstream* file, T* array, unsigned int n);
 
 int main() {
-    unsigned int n[] = {4};
+    unsigned int n[] = {1000};
     FLOAT omega[] = {0.01, 0.5, 1, 5};          // Oscillator frequency
-    FLOAT _r = 0, r_ = 1E7;                       // Solution interval
+    FLOAT _r = 0, r_ = 1E7;                     // Solution interval
+    ofstream timefile;
+    timefile.open("time.dat");
 
     for(unsigned int i = 0; i < ARRAY_SIZE(n); i++) {
 
@@ -46,37 +50,84 @@ int main() {
         FLOAT h = (rho_-_rho)/(n[i]+1);         // Step length
         FLOAT* rho = new FLOAT[n[i]+2];         // the variable rho
         FLOAT* d = new FLOAT[n[i]];             // Diagonal elements
-        FLOAT* e = new FLOAT[n[i]];             // Off diagonal elements
-        FLOAT** z = new FLOAT*[n[i]];           // Eigenvector;
 
         rho[n[i]+1] = rho_;
         for(unsigned int j = 0; j < n[i]; j++) {
             rho[j+1] = (j+1)*h;
             d[j] = 2 + 0.5*k*pow(h*alpha*rho[j+1],2);
-            e[j] = -1;
-            z[j] = new FLOAT[n[i]];
         }
-        auto matrix = Matrix<MatrixType::Symmetric, FLOAT>(n[i]);
-        matrix.Diagonal(0, d);
-        matrix.Diagonal(1, -1);
+        timefile << n[i] << " & ";
+        auto eigv = Matrix<MatrixType::Square, FLOAT>(n[i]);    // Eigenvectors
+        eigv.Diagonal(0,1);
 
-        FLOAT p1 = d[0];
-        FLOAT p2 = d[1];
-        FLOAT p3 = d[2];
-        FLOAT p4 = d[3];
-        tqli(d, e, n[i], z);
-        FLOAT d1 = d[0];
-        FLOAT d2 = d[1];
-        FLOAT d3 = d[2];
-        FLOAT d4 = d[3];
+        auto matrix = Matrix<MatrixType::tqli, FLOAT>(n[i]);    // Benchmark, tqli in lib.cpp
+        matrix.Diagonal(0, d);
+        matrix.Diagonal(1,-1);
+        clock_t t0 = clock();
+        matrix.tqli(eigv);
+        timefile << (double)(clock()-t0)/CLOCKS_PER_SEC << " & ";
+        //MatrixCout(matrix);
+        //MatrixCout(eigv);
+
+        if(n[i] <= 100) {
+            auto matrix1 = Matrix<MatrixType::Square, FLOAT>(n[i]);
+            matrix1.Diagonal(0, d);
+            matrix1.Diagonal(1,-1);
+            matrix1.Diagonal(-1,-1);
+            //MatrixCout(matrix1);
+            t0 = clock();
+            matrix1.JacobiMethod(1e-3,1);
+            timefile << (double)(clock()-t0)/CLOCKS_PER_SEC << " & ";
+        } else
+            timefile << "- & ";
+
+        if(n[i] <= 100) {
+            auto matrix2 = Matrix<MatrixType::Symmetric, FLOAT>(n[i]);
+            matrix2.Diagonal(0, d);
+            matrix2.Diagonal(1,-1);
+            //MatrixCout(matrix2);
+            t0 = clock();
+            matrix2.JacobiMethod(1e-3,1);
+            timefile << (double)(clock()-t0)/CLOCKS_PER_SEC << " & ";
+        } else
+            timefile << "- & ";
+
+        auto matrix3 = Matrix<MatrixType::Symmetric, FLOAT>(n[i]);
+        matrix3.Diagonal(0, d);
+        matrix3.Diagonal(1,-1);
+        t0 = clock();
+        matrix3.JacobiMethodFaster(1e-3,1);
+        timefile << (double)(clock()-t0)/CLOCKS_PER_SEC << " & " << endl;
+
+        auto matrix4 = Matrix<MatrixType::Symmetric, FLOAT>(n[i]);
+        matrix4.Diagonal(0, d);
+        matrix4.Diagonal(1,-1);
+        t0 = clock();
+        matrix4.JacobiMethodFastest(1e-3,1);
+        timefile << (double)(clock()-t0)/CLOCKS_PER_SEC << " \\\\" << endl;
+
+        auto matrix5 = Matrix<MatrixType::Symmetric, FLOAT>(5);
+        matrix5(0,0) = 1;
+        matrix5(0,1) = 2;
+        matrix5(0,2) = 3;
+        matrix5(0,3) = 4;
+        matrix5(0,4) = 5;
+        matrix5(1,1) = 6;
+        matrix5(1,2) = 7;
+        matrix5(1,3) = 8;
+        matrix5(1,4) = 9;
+        matrix5(2,2) = 10;
+        matrix5(2,3) = 11;
+        matrix5(2,4) = 12;
+        matrix5(3,3) = 13;
+        matrix5(3,4) = 14;
+        matrix5(4,4) = 15;
+        //MatrixCout(matrix4);
 
         delete [] rho;
         delete [] d;
-        delete [] e;
-        for(unsigned int j = 0; j < n[i]; j++)
-            delete [] z[j];
-        delete [] z;
     }
+    timefile.close();
     return 0;
 }
 template<class T> T* CopyOfArray(T* arr, unsigned int n) {
